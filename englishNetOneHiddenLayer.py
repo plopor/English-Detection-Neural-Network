6eff3a3fecbf8f12e9b5364be0d8e205cc1ddbfb
plopor <bowen.luo@gmail.com>
2019-01-03 21:53:01 -0500

    added initial training examples, mere 60, mere 4000 iterations

---
+++ englishNetOneHiddenLayer.py
@@ -0,0 +1,171 @@
+import numpy as np
+import random
+import copy
+
+class neuron ():
+    def __init__(self, inputLayer, weights):
+        self.output = 0
+        for x in range (len(inputLayer)):
+            self.output += (float(inputLayer[x]) * weights[x])
+        self.output = 1 / (1 + np.exp(-self.output))
+        self.inputLayer = inputLayer
+        self.weights = weights
+
+class outNeuron ():
+    def __init__(self, hiddenLayer, weights):
+        self.output = 0
+        for x in range (len(hiddenLayer)):
+            self.output += (hiddenLayer[x].output * weights[x])
+        self.output = 1 / (1 + np.exp(-self.output))
+        self.hiddenLayer = hiddenLayer
+        self.weights = weights
+
+maxLength = 10
+engLib = open("english.txt", "r")
+answers = open("answers.txt", "r")
+learnRate = 1
+
+def convertToVector(los, maxLength):
+    vecList = []
+    for word in los.split():
+        vec = ''
+        n = len(word)
+        for i in range(n):
+            charNum = ord(word[i])-97
+            value = (str(0) * charNum) + str(1) + str(0) * (25 - charNum)
+            vec = vec + value
+        if n < maxLength:
+            vec = vec + str(0) * 26 * (maxLength - n)
+        vecList.append(vec)
+    print(len(vecList))
+    print(vecList)
+    return vecList
+
+def makeLayer(inputLayer, weights, neuronNum):
+    outList = []
+    for x in range (neuronNum):
+        outList.append(neuron(inputLayer, weights[x]))
+    return outList
+
+def initWeights(inputsFromLayer, numberOfNeurons):
+    neurons = []
+    for y in range (numberOfNeurons):
+        weights = []
+        for x in range (inputsFromLayer):
+            weights.append(random.uniform(-1, 1))
+        neurons.append(weights)
+    return neurons
+
+def backPropOut(ideal, outputNeuron):
+    error = -2*(ideal - outputNeuron.output)
+    adjust = error * outputNeuron.output * (1 - outputNeuron.output)
+    for x in range (len(outputNeuron.hiddenLayer)):
+        outputNeuron.weights[x] -= outputNeuron.hiddenLayer[x].output * adjust
+    return adjust
+
+def backPropInput(passBack, saveWeights, outLayerProp):
+    for x in range (len (outLayerProp)):
+        temp = []
+        adjust = passBack * saveWeights[x] * outLayerProp[x].output * (1 - outLayerProp[x].output)
+        for y in range (len (outLayerProp[x].inputLayer)):
+            temp.append(outLayerProp[x].weights[y] - float(outLayerProp[x].inputLayer[y]) * adjust)
+        outLayerProp[x].weights = temp
+
+train = convertToVector(engLib.read(), 10)
+trainOut = answers.read()
+iterations = 4000
+averageWeights = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+averageOut = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+testWeights = initWeights(260, 10)
+print(testWeights[0])
+testOutWeights = initWeights(10, 1)
+print (testOutWeights)
+
+
+for word in range (len (train)):
+    print("-------------------------------------------------------------")
+    for iterate in range (iterations):
+        #the forwards feed
+        outputs = makeLayer(train[word], testWeights, 10)
+
+        outputNeuron = outNeuron(outputs, testOutWeights[0])
+
+        print(outputNeuron.output)
+
+        #the backwards pass
+        saveWeights = copy.deepcopy(outputNeuron.weights)
+        passBack = backPropOut(float(trainOut[word]), outputNeuron)
+
+        backPropInput(passBack, saveWeights, outputs)
+
+        #updating input weights for next iteration
+        testWeights = []
+        for update in range (len (outputs)):
+            testWeights.append(outputs[update].weights)
+        testOutWeights = [outputNeuron.weights]
+
+    print("altered hidden layer weights:")
+    print(outputs[0].weights)
+    print(outputs[1].weights)
+    print(outputs[2].weights)
+    print(outputs[3].weights)
+    print(outputs[4].weights)
+    print(outputs[5].weights)
+    print(outputs[6].weights)
+    print(outputs[7].weights)
+    print(outputs[8].weights)
+    print(outputs[9].weights)
+    print("altered output weights:")
+    print(outputNeuron.weights)
+
+    for neurons in range (len (outputs)):
+        for weights in range (len (outputs[neurons].weights)):
+            averageWeights[neurons][weights] += outputs[neurons].weights[weights]
+    for outWeights in range (len (outputNeuron.weights)):
+        averageOut[outWeights] += outputNeuron.weights[outWeights]
+
+print("summed hidden layer weights:")
+print(averageWeights[0])
+print(averageWeights[1])
+print(averageWeights[2])
+print(averageWeights[3])
+print(averageWeights[4])
+print(averageWeights[5])
+print(averageWeights[6])
+print(averageWeights[7])
+print(averageWeights[8])
+print(averageWeights[9])
+print("summed output weights:")
+print(averageOut)
+
+for neurons in range (len (outputs)):
+    for weights in range (len(averageWeights[neurons])):
+        averageWeights[neurons][weights] = learnRate * averageWeights[neurons][weights] / len (train) # change as input amount changes
+for outWeights in range (len (averageOut)):
+    averageOut[outWeights] = learnRate * averageOut[outWeights] / len (train) #change as input amount changes
+
+print("averaged hidden layer weights:")
+print(averageWeights[0])
+print(averageWeights[1])
+print(averageWeights[2])
+print(averageWeights[3])
+print(averageWeights[4])
+print(averageWeights[5])
+print(averageWeights[6])
+print(averageWeights[7])
+print(averageWeights[8])
+print(averageWeights[9])
+print("averaged output weights:")
+print(averageOut)
+
+while True:
+    word = input("Word: ")
+    word = convertToVector(word, maxLength)
+    guessLayer = makeLayer (word[0], averageWeights, 10)
+    guess = outNeuron (guessLayer, averageOut)
+    print(guess.output)
+    
+
+    
+
