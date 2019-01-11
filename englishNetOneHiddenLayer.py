e1b6aba3b7c2e34312a17e4be37edf602c94d67b
plopor <bowen.luo@gmail.com>
2019-01-11 00:04:40 -0500

    added comments

--- englishNetOneHiddenLayer.py
+++ englishNetOneHiddenLayer.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 import copy
 
+#constructor for a hidden layer neuron
 class neuron ():
     def __init__(self, inputLayer, weights):
         self.output = 0
@@ -11,6 +12,7 @@ class neuron ():
         self.inputLayer = inputLayer
         self.weights = weights
 
+#constructor for output neuron having been given hidden layer as the input layer
 class outNeuron ():
     def __init__(self, hiddenLayer, weights):
         self.output = 0
@@ -23,89 +25,100 @@ class outNeuron ():
 maxLength = 15 # alter max length of input words
 engLib = open("english.txt", "r")
 answers = open("answers.txt", "r")
-learnRate = 1
 
+#takes a having been converted text file, list of words and turns each word into a series of 0 and 1,
+#each letter being 26 0s or 1s depending on the letter. The list is 26 * (max amount of letters per word) in length
 def convertToVector(los, maxLength):
     vecList = []
-    for word in los.split():
+    for word in los.split(): #make a new word vector at a space or newline
         vec = ''
-        n = len(word)
-        for i in range(n):
-            charNum = ord(word[i])-97
-            value = (str(0) * charNum) + str(1) + str(0) * (25 - charNum)
+        n = len(word) #find length of current word
+        for i in range(n): #for each character in current word
+            charNum = ord(word[i])-97 #takes the unicode of current character and arranges it from 0 (a is 0, b is 1 etc)
+            value = (str(0) * charNum) + str(1) + str(0) * (25 - charNum) #in a list of 25 0s, the 1 is placed at its correspinding letter's location
             vec = vec + value
         if n < maxLength:
-            vec = vec + str(0) * 26 * (maxLength - n)
+            vec = vec + str(0) * 26 * (maxLength - n) #fills in the rest of the unused letter spaces for words shorter than the max with 0s
         vecList.append(vec)
+    print("Number of training cases:")
     print(len(vecList))
+    print("Training cases:")
     print(vecList)
     return vecList
 
+#makes a layer of hidden neurons
 def makeLayer(inputLayer, weights, neuronNum):
     outList = []
-    for x in range (neuronNum):
+    for x in range (neuronNum): #makes a specified amount of hidden neurons
         outList.append(neuron(inputLayer, weights[x]))
     return outList
 
+#create a random set of weights for initial use to be altered by training
 def initWeights(inputsFromLayer, numberOfNeurons):
     neurons = []
-    for y in range (numberOfNeurons):
+    for y in range (numberOfNeurons): #create a set of weights for each hidden layer neuron
         weights = []
-        for x in range (inputsFromLayer):
+        for x in range (inputsFromLayer): #create random weight values for each incoming value
             weights.append(random.uniform(-1, 1))
         neurons.append(weights)
     return neurons
 
+#back prop for output neuron
 def backPropOut(ideal, outputNeuron):
-    error = -2*(ideal - outputNeuron.output)
-    adjust = error * outputNeuron.output * (1 - outputNeuron.output)
-    for x in range (len(outputNeuron.hiddenLayer)):
+    error = -2*(ideal - outputNeuron.output) #calculate error
+    adjust = error * outputNeuron.output * (1 - outputNeuron.output) #calculate adjustment factor
+    for x in range (len(outputNeuron.hiddenLayer)): #apply changes to weight for each incoming weight to output neuron
         outputNeuron.weights[x] -= outputNeuron.hiddenLayer[x].output * adjust
     return adjust
 
+#back propogation for the hidden layer
 def backPropInput(passBack, saveWeights, outLayerProp):
-    for x in range (len (outLayerProp)):
+    for x in range (len (outLayerProp)): #apply algorithm for each neuron in hidden layer
         temp = []
-        adjust = passBack * saveWeights[x] * outLayerProp[x].output * (1 - outLayerProp[x].output)
-        for y in range (len (outLayerProp[x].inputLayer)):
+        adjust = passBack * saveWeights[x] * outLayerProp[x].output * (1 - outLayerProp[x].output) #adjustment to make on current neuron
+        for y in range (len (outLayerProp[x].inputLayer)): #apply algorithm for each incoming weight in current neuron
             temp.append(outLayerProp[x].weights[y] - float(outLayerProp[x].inputLayer[y]) * adjust)
         outLayerProp[x].weights = temp
 
-train = convertToVector(engLib.read(), 10)
-trainOut = answers.read()
-iterations = 1000
+#initialize some inputs
+train = convertToVector(engLib.read(), 10) #write the txt file into an array
+trainOut = answers.read() #write answers that correspond to txt file into array
+iterations = 1000 #set number of iterations
+#initialize base average weights for hidden, then output layers
 averageWeights = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
 averageOut = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+learnRate = 2 #set learning rate, play around to alter results
 
 testWeights = initWeights(390, 10) # change word length as fit
 #print(testWeights[0])
 testOutWeights = initWeights(10, 1)
 #print (testOutWeights)
 
+#the training process
 print("Words processed:")
-for word in range (len (train)):
+for word in range (len (train)): #loops through each word in the sample list
     print("-------------------------------------------------------------")
-    for iterate in range (iterations):
+    for iterate in range (iterations): #iterates each word an amount of time specified by iterations
         #the forwards feed
-        outputs = makeLayer(train[word], testWeights, 10)
+        outputs = makeLayer(train[word], testWeights, 10) #make the hidden layer of 10 neurons
 
-        outputNeuron = outNeuron(outputs, testOutWeights[0])
+        outputNeuron = outNeuron(outputs, testOutWeights[0]) #make the output neuron, contains the guess as well
 
         #print(outputNeuron.output)
 
         #the backwards pass
-        saveWeights = copy.deepcopy(outputNeuron.weights)
-        passBack = backPropOut(float(trainOut[word]), outputNeuron)
+        saveWeights = copy.deepcopy(outputNeuron.weights) #keep a copy of old weights to use for hidden layer propogation
+        passBack = backPropOut(float(trainOut[word]), outputNeuron) #back-propogate through the output neuron, weights are updated
 
-        backPropInput(passBack, saveWeights, outputs)
+        backPropInput(passBack, saveWeights, outputs) #back-propogate through the hidden layer, weights are updated
 
-        #updating input weights for next iteration
+        #updating input weights for use in next iteration
         testWeights = []
         for update in range (len (outputs)):
             testWeights.append(outputs[update].weights)
         testOutWeights = [outputNeuron.weights]
 
-    #print("altered hidden layer weights:")
+    #print("altered hidden layer weights:") #prints out the hidden layer's weights for current word
     #print(outputs[0].weights)
     #print(outputs[1].weights)
     #print(outputs[2].weights)
@@ -116,17 +129,18 @@ for word in range (len (train)):
     #print(outputs[7].weights)
     #print(outputs[8].weights)
     #print(outputs[9].weights)
-    #print("altered output weights:")
+    #print("altered output weights:") #prints out the output neuron's weights for current word
     #print(outputNeuron.weights)
     print(word)
 
+    #constructs a sum of all the weights for the words in the bank
     for neurons in range (len (outputs)):
         for weights in range (len (outputs[neurons].weights)):
-            averageWeights[neurons][weights] += outputs[neurons].weights[weights]
+            averageWeights[neurons][weights] += outputs[neurons].weights[weights] #adds current hidden layer weights to the sum of previous weights
     for outWeights in range (len (outputNeuron.weights)):
-        averageOut[outWeights] += outputNeuron.weights[outWeights]
+        averageOut[outWeights] += outputNeuron.weights[outWeights] #adds current output neuron weights to the sum of previous weights
 
-print("summed hidden layer weights:")
+print("summed hidden layer weights:") #display resulted summed weights for the hidden layer after training
 print(averageWeights[0])
 print(averageWeights[1])
 print(averageWeights[2])
@@ -137,16 +151,19 @@ print(averageWeights[6])
 print(averageWeights[7])
 print(averageWeights[8])
 print(averageWeights[9])
-print("summed output weights:")
+print("summed output weights:") #display resulted summed weights for output neuron post training
 print(averageOut)
 
+
+#averages each weight by total amount of test words
 for neurons in range (len (outputs)):
     for weights in range (len(averageWeights[neurons])):
-        averageWeights[neurons][weights] = learnRate * averageWeights[neurons][weights] / len (train)
+        averageWeights[neurons][weights] = learnRate * averageWeights[neurons][weights] / len (train) #for hidden layer
 for outWeights in range (len (averageOut)):
-    averageOut[outWeights] = learnRate * averageOut[outWeights] / len (train)
+    averageOut[outWeights] = learnRate * averageOut[outWeights] / len (train) #for output neuron
 
-print("averaged hidden layer weights:")
+#display resulted averaged weights
+print("averaged hidden layer weights:") #for hidden layer
 print(averageWeights[0])
 print(averageWeights[1])
 print(averageWeights[2])
@@ -157,9 +174,10 @@ print(averageWeights[6])
 print(averageWeights[7])
 print(averageWeights[8])
 print(averageWeights[9])
-print("averaged output weights:")
+print("averaged output weights:") #for output neuron
 print(averageOut)
 
+#prompt user to give the net words to guess
 while True:
     word = input("Word: ")
     word = convertToVector(word, maxLength)
